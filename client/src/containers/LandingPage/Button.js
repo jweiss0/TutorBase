0b0a4d3523ce2328aa4f9d9f4e629e1b707e1679
tamckinney <91158277+tamckinney@users.noreply.github.com>
2021-12-07 18:40:48 -0500

    Update Button.js
    Button style and route fix

--- client/src/containers/LandingPage/Button.js
+++ client/src/containers/LandingPage/Button.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import './Button.css'
+import React, { useState } from 'react';
+import './Button.css';
 import {Link} from 'react-router-dom';
-const STYLES = ['btn--primary', 'btn--outline',]
+const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
+
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
     return (
-        <Link to='/log-in' className='btn-mobile' href = "/login">
+        <Link to='/login' className='btn-mobile'>
             <button
-            className={'btn ${checkButtonStyle} ${checkButtonSize}'}
-            onClick={onClick}
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick="window.location.href='/login'"
             type={type}>
                 {children}
             </button>
         </Link>
-    )
+    );
 };
